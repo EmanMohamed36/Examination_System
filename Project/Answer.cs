4ca357cd984a4022f93ce7be852b21e73e649bf4
EmanMohamed36 <ei3377898@gmail.com>
2025-08-19 21:11:59 +0300

    upload All project of Examination System

---
+++ Project/Answer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam02
+{
+    internal class Answer
+    {
+        private int id;
+        public string? Text { protected set; get; }
+
+        public int Id
+        { 
+            get { return id; }
+            set { id = value < 0? 0:value; }
+        }
+        public Answer(int _Id , string _Text)
+        { 
+            Id = _Id;
+            Text = _Text;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {id} , Text: {Text}";
+        }
+    }
+}
---
+++ Project/Subject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Exam02
+{
+    internal class Subject
+    {
+        protected int id;
+        public string Name { protected set;  get; }
+
+        public List<Exam> finalExamsList { get; set; }
+        public List<Exam> practicalExamsList { get; set; }
+        public Subject(int _Id, string _Name)
+        {
+            Id = _Id;
+            Name = _Name;
+            finalExamsList = new List<Exam>();
+            practicalExamsList = new List<Exam>();
+        }
+        public int Id
+        {
+            set { id = value < 0 ? 0 : value; }
+            get { return id; }
+        }
+        public override string ToString()
+        {
+            
+            return $"Id: {Id}  Name: {Name}";
+        }
+    }
+}
